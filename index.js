efacb3a7a04589fa6d9f677890177fc1a60cf22a
PoHsin <ok118023@gamil.com>
2019-07-26 15:05:44 +0800

    YY

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const DRIVER = "geckodriver"; //chromedriver
-const BROWSER = "firefox"; //chrome
+
+
+const DRIVER = "chromedriver"; //chromedriver
+const BROWSER = "chrome"; //chrome
 
 var webdriver = require("selenium-webdriver");
 require(DRIVER);
@@ -15,10 +17,25 @@ async function execute() {
     var driver = builder.build();
 
     await driver.get("https://www.youtube.com/?gl=TW&hl=zh-TW");
+    // var soup = new JSSoup(data);
+    // soup.findAll(driver)
     var searchBox=await getElement(driver, webdriver.By.name("search_query"), 3000);
     await searchBox.sendKeys("復仇者");
     var searchButton=await getElement(driver, webdriver.By.id("search-icon-legacy"), 3000);
     await searchButton.click();
+    let c = await getElement(driver, webdriver.By.className("ytd-video-renderer"), 3000);
+    let d = await c.findElement(webdriver.By.tagName("a"));
+    var h = await d.getAttribute("href");
+
+    await driver.get("https://www.safetoconvert.com/convert-youtube-video");
+    let s = await getElement(driver, webdriver.By.id("search"), 3000);
+    await s.sendKeys(h);
+    let b = await getElement(driver, webdriver.By.className("fa-search"), 3000);
+    await b.click();
+    let k = await getElement(driver, webdriver.By.className("btn-success"), 3000);
+    await k.click();
+    driver.close();
+    
 }
 
 execute();
